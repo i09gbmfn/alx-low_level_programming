aa8e66dcd81179ad1532cf5d685127653d0b1648
AHMED IBRAHIM <a15s108@gmail.com>
2023-08-25 16:09:52 -0700

    0-isupper betty fix

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,3 +1,9 @@
+/**
+ * _isupper - Checks for uppercase character
+ * @c: The character to be checked
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
